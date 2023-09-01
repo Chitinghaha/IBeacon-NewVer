c3b8d86fd6e4a37767ad8b3958ff21fd8d55f46d
leobkchen <bangkangchen@gmail.com>
2023-09-01 14:11:18 +0800

    Initial Commit

---
+++ VoiceOveriBeacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceOveriBeacon
+//
+//  Created by 陳邦亢 on 2023/9/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
